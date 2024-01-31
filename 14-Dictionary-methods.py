e5d5bbc93b928f89deae888addb5d57d3ed6454a
Priyojit <priyojitsingha01@gmail.com>
2024-01-31 16:01:10 +0530

    dictionary function

---
+++ 14-Dictionary-methods.py
@@ -0,0 +1,27 @@
+dict1={
+    "A":"Apple",
+    "B":"Ball",
+    "C":"Cat"
+}
+dict2={
+    "D":"Doll",
+    "E":"Elephant"
+}
+
+# to add the elements of a dictionary to other, use update()
+# dict1.update(dict2)
+# print(dict1)
+
+# to remove a particular key-value pair, use pop()
+# dict1.pop("B")
+# del dict1["C"]
+# print(dict1)
+
+# to delete the entire dictionary, use del like this
+# del dict2
+
+# to remove the last key-value pair, use popitem()
+# dict1.popitem()
+# print(dict1)
+
+
